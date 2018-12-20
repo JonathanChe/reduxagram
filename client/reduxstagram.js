6fb0ee79626a7d2efa515b1d5d2b09db69be591d
Jonathan <jonathan.che@outlook.com>
2018-12-19 20:07:50 -0800

    Provider wraps around top level Router

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,22 +2,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 
 // rel path imports
 import Main from './components/Main';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
+import store, { history } from './store';
 
 // import css
 import css from './styles/style.styl';
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="view/:postId" component={Single}></Route>
-    </Route>
-  </Router>
-)
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+);
 
 render(router, document.getElementById('root'));
