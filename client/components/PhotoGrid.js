7d14e799d5355feeec4e9aeb667a56cbe7a00659
Jonathan <jonathan.che@outlook.com>
2018-12-19 18:08:57 -0800

    photogrid & single components created

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const PhotoGrid = () => {
+  return (
+    <div className="photo-grid">
+      I'm the photogrid
+    </div>
+  )
+}
+
+export default PhotoGrid;
